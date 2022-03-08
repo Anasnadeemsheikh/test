43688f2797166024b8b00c0780ecd6dccc470cdc
Muhammad Anas Nadeem <anasnadeemsheikh@gmail.com>
2022-03-08 12:28:35 +0500

    Initials

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allreducers from './reducers'
+import {Provider} from 'react-redux'
+
+const store = createStore(allreducers);
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
